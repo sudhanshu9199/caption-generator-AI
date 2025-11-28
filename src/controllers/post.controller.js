024ef8320173b90d7050a24c38bb40b3966e0d59
SUDHANSHU GHOSH <shudhanshukumar9713@gmail.com>
2025-11-28 17:09:15 +0530

    day 141 done ai caption creation

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,19 +1,36 @@
 // post.controller.js
 const postModel = require("../models/post.model.js");
 const aiService = require("../service/ai.service.js");
+const UploadFile = require('../service/storage.service.js');
+const {v4: uuidv4} = require('uuid');
 
 async function createPostController(req, res) {
     const file = req.file;
     console.log('File received:', file);
 
-    const base64Image = new Buffer.from(file.buffer).toString('base64');
+    const base64Image = Buffer.from(file.buffer).toString('base64');
 
     const caption = await aiService(base64Image);
     console.log('Generate caption:', caption);
 
-    res.json({
-        caption
+    const dataUri = `data:${file.mimetype};base64,${base64Image}`;
+    const result = await UploadFile(dataUri, `${uuidv4()}`);
+
+    const post = await postModel.create({
+        caption,
+        image: result.url,
+        user: req.user._id
     })
+
+    res.status(201).json({
+        message: 'post created successfully',
+        post
+    })
+
+    // res.json({
+    //     caption,
+    //     result
+    // })
     
 }
 module.exports = { createPostController };
--- src/service/storage.service.js
+++ src/service/storage.service.js
@@ -1,17 +1,20 @@
-import ImageKit from '@imagekit/nodejs';
+const {ImageKit} = require('@imagekit/nodejs');
 
 const client = new ImageKit({
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY, // This is the default and can be omitted
+//   publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+//   urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT
 });
 
 async function uploadFile(file, filename) {
     const response = await client.files.upload({
       file: file,
       fileName: filename,
+      folder: 'cohort_AI_Social'
     });
+    // console.log("This response ->:",response);
 
     return response;
 }
 
-console.log(response);
 module.exports = uploadFile;
