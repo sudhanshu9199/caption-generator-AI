f791cfd617bf4fd25d829a02314443e5b4df5dee
SUDHANSHU GHOSH <shudhanshukumar9713@gmail.com>
2025-12-14 16:14:43 +0530

    fixed backend url problem.

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -1,17 +1,19 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const authRoutes = require('./routes/auth.route.js');
-const postRoutes = require('./routes/post.route.js');
-const cors = require('cors');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const authRoutes = require("./routes/auth.route.js");
+const postRoutes = require("./routes/post.route.js");
+const cors = require("cors");
 const app = express();
 
-app.use(cors({
-    origin: 'http//localhost:5173',
+app.use(
+  cors({
+    origin: "http://localhost:5173",
     credentials: true,
-}))
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api/auth', authRoutes);
-app.use('/api/posts', postRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
 
 module.exports = app;
