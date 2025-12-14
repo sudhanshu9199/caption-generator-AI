1e725e812725e685c30b298152df41e17eb611ed
SUDHANSHU GHOSH <shudhanshukumar9713@gmail.com>
2025-12-14 16:24:14 +0530

    create a profile page and added in the main route

--- FRONTEND/src/router/MainRouter.jsx
+++ FRONTEND/src/router/MainRouter.jsx
@@ -3,11 +3,13 @@ import { lazy } from 'react';
 const Dashboard = lazy(() => import('../Pages/Dashboard/Dashboard'))
 const Login = lazy(() => import('../Pages/Login/Login'))
 const Register = lazy(() => import('../Pages/Register/Register'))
+const Profile = lazy(() => import('../Pages/Profile/Profile'))
 
 const MainRouter = () => {
   return (
     <Routes>
         <Route path='/' element={<Dashboard />} />
+        <Route path='/profile' element={<Profile />} />
         <Route path='/auth/login' element={<Login />} />
         <Route path='/auth/register' element={<Register />} />
     </Routes>
