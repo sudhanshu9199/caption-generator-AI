bf7d6df2e01f449f3ef6ebeb746d370309f30523
SUDHANSHU GHOSH <shudhanshukumar9713@gmail.com>
2025-12-14 14:26:24 +0530

    Backend: CORS added

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -2,7 +2,13 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const authRoutes = require('./routes/auth.route.js');
 const postRoutes = require('./routes/post.route.js');
+const cors = require('cors');
 const app = express();
+
+app.use(cors({
+    origin: 'http//localhost:5173',
+    credentials: true,
+}))
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api/auth', authRoutes);
