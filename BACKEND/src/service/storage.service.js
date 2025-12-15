13cd11836d7925da9c2b8e0f2436c729c9076cc5
SUDHANSHU GHOSH <shudhanshukumar9713@gmail.com>
2025-12-15 23:13:38 +0530

    Uncomment publicKey and urlEndpoint in ImageKit client configuration

--- BACKEND/src/service/storage.service.js
+++ BACKEND/src/service/storage.service.js
@@ -2,8 +2,8 @@ const {ImageKit} = require('@imagekit/nodejs');
 
 const client = new ImageKit({
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY, // This is the default and can be omitted
-//   publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
-//   urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT
 });
 
 async function uploadFile(file, filename) {
